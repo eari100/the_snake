c78d13804e85df4c5ee08a916101384b9b950aaf
Jaewook Choi <eari100@naver.com>
2024-05-18 18:13:55 +0900

    feat: 스네이크 1초마다 자동 이동되도록 추가

--- app/components/Board.tsx
+++ app/components/Board.tsx
@@ -1,21 +1,65 @@
 'use client'
 
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 interface BoardProps {
     width: number
     height: number
 }
 
+interface SnakePosition {
+    y: number
+    x: number
+}
+
+enum SnakeDirection {
+    UP = 'UP',
+    DOWN = 'DOWN',
+    LEFT = 'LEFT',
+    RIGHT = 'RIGHT'
+}
+
 const Board: React.FC<BoardProps> = ({ width, height }) => {
-    const initialSnakePosition = { y: Math.floor(height / 2), x: Math.floor(width / 2) }
-    const [snake, setSnake] = useState<{ y: number, x: number }[]>([initialSnakePosition])
+    const initialSnakePosition: SnakePosition[] = [{ y: Math.floor(height / 2), x: Math.floor(width / 2) }]
+    const [snake, setSnake] = useState<SnakePosition[]>(initialSnakePosition)
+    const [snakeDirection, setSnakeDirection] = useState<SnakeDirection>(SnakeDirection.DOWN)
+
+    const moveSnake = () => {
+        setSnake(prevSnake => {
+            const { y, x } = prevSnake[0]
+            let newHead: { y: number, x: number }
+
+            switch (snakeDirection) {
+                case SnakeDirection.UP:
+                    newHead = { y: y - 1, x }
+                    break
+                case SnakeDirection.DOWN:
+                    newHead = { y: y + 1, x }
+                    break
+                case SnakeDirection.LEFT:
+                    newHead = { y, x: x - 1 }
+                    break
+                case SnakeDirection.RIGHT:
+                    newHead = { y, x: x + 1 }
+                    break
+                default:
+                    newHead = { y, x }
+            }
+
+            const newSnake = [newHead, ...snake.slice(0, -1)]
+            return newSnake
+        })
+    }
+
+    useEffect(() => {
+        const intervalId = setInterval(moveSnake, 1000)
+        return () => clearInterval(intervalId)
+    }, [])
 
     const renderCells = () => {
         const cells = []
         for (let row = 0; row < height; row++) {
             for (let col = 0; col < width; col++) {
-                // Check if the cell is part of the snake
                 const isSnakeCell = snake.some(({ y, x }) => y === row && x === col)
                 cells.push(
                     <div
@@ -24,7 +68,7 @@ const Board: React.FC<BoardProps> = ({ width, height }) => {
                             border: "1px solid #888",
                             width: "20px",
                             height: "20px",
-                            backgroundColor: isSnakeCell ? "green" : "transparent" // Set background color to green if it's part of the snake
+                            backgroundColor: isSnakeCell ? "green" : "transparent"
                         }}
                     />
                 )
